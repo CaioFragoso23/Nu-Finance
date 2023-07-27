d3cd735405b8e2e05bb63523d7b83dc081464d83
CaioFragoso23 <caiocesarfragoso@gmail.com>
2023-07-26 21:18:30 -0300

    adding project info on readme

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 https://vercel.com/caiofragoso23/react-entrega-s1-nu-kenzie-caio-fragoso23/CqQ3MmJDsH9SX61JZqycpNyTQohz
 https://react-entrega-s1-nu-kenzie-caio-fragoso23-caiofragoso23.vercel.app
+
+Esse app foi utilizado para aprender a montar um app usando React, e aprender a utilizar o useState
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -14,6 +14,7 @@ export const Form = ({listTransactions, setListTransactions, listValue, setListV
         setListTransactions([...listTransactions, transaction])
         setListValue([...listValue, transaction])}} >
             <div className="filler" >
+                <p>Descrição</p>
                 <input className="input-description transactionDescription" placeholder="Descrição da transação" value={transaction.description} onChange={(event) => setTransaction({...transaction, description: event.target.value})} type="text" required ></input>
                 <p className="subtitle">Ex: Compra de roupas</p>
             </div>
@@ -23,7 +24,7 @@ export const Form = ({listTransactions, setListTransactions, listValue, setListV
                     <input className="input-number transactionInputs" placeholder="Valor da transação." value={transaction.value} type="number" onChange={(event) => setTransaction({...transaction, value: parseInt(event.target.value)})} required />
                 </div>
                 <div className="filler">
-                <p>Tipo de transação</p>
+                <p>Tipo de valor</p>
                 <select className="input-select transactionInputs" onChange={(event) => setTransaction({...transaction, type: event.target.value})} name="tipoPgto" id="tipoPgto" required >
                     <option value="">Selecionar tipo de transação.</option>
                     <option value="entrada">Entrada</option>
